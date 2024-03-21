cd9bad6c8d033c0c9de5b5cafb0e96108bed5dd9
Bewizi <toluwalase.bewaji@yahoo.com>
2024-03-21 11:54:49 +0100

    bewizi

---
+++ app.js
@@ -0,0 +1,8 @@
+const cardInfo = {
+  cardName: "John Smith",
+  cardNumber: `${123}-${456}-${789}-${468}-${378}`,
+};
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+});
